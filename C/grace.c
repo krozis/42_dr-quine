f47c4f1873dcf6f091ce38b74b5ed418d0fdac5b
stelie <stelie@student.42.fr>
2026-05-28 16:02:04 +0200

    added () to correspond to the subject

--- C/grace.c
+++ C/grace.c
@@ -4,8 +4,8 @@
 	Another One Writes the Rust
 */
 
-#define S "#include <stdio.h>%c%c/*%c%cAnother One Writes the Rust%c*/%c%c#define S %c%s%c%c#define N %cGrace_kid.c%c%c#define MAIN int main(void){FILE*f=fopen(N,%cw%c);fprintf(f,S,10,10,10,9,10,10,10,34,S,34,10,34,34,10,34,34,10,10,10);fclose(f);return(0);}%c%cMAIN%c"
+#define S "#include <stdio.h>%c%c/*%c%cAnother One Writes the Rust%c*/%c%c#define S %c%s%c%c#define N %cGrace_kid.c%c%c#define MAIN() int main(void){FILE*f=fopen(N,%cw%c);fprintf(f,S,10,10,10,9,10,10,10,34,S,34,10,34,34,10,34,34,10,10,10);fclose(f);return(0);}%c%cMAIN()%c"
 #define N "Grace_kid.c"
-#define MAIN int main(void){FILE*f=fopen(N,"w");fprintf(f,S,10,10,10,9,10,10,10,34,S,34,10,34,34,10,34,34,10,10,10);fclose(f);return(0);}
+#define MAIN() int main(void){FILE*f=fopen(N,"w");fprintf(f,S,10,10,10,9,10,10,10,34,S,34,10,34,34,10,34,34,10,10,10);fclose(f);return(0);}
 
-MAIN
+MAIN()
